d92c1c10af2f3df0afc93f463f62560828c9b453
Hritik Arora <hritikarora875@gmail.com>
2021-05-10 14:55:26 +0530

    second commit

--- src/components/ResultDeatils.js
+++ src/components/ResultDeatils.js
@@ -1,16 +1,27 @@
 import React from 'react';
-import {View,Text,StyleSheet,Image} from 'react-native'
-import ResultsList from './ResultList';
+import {View,Text,StyleSheet,Image} from 'react-native';
+
 
 const ResultDetails = ({result}) => {
+    //console.log(result)
     return <View>
-        <Text>{result.name}</Text>
+        <Image style={styles.image} source={{uri: result.image_url }}/>
+        <Text  style={styles.name}>{result.name}</Text>
+        <Text>{result.rating} Stars, {result.review_count} Reviews</Text>
     </View>
 };
 
 
 const styles = StyleSheet.create ({
-
+image: {
+    width: 250,
+    height:120,
+    borderRadius:4
+},
+    name: {
+       fontWeight:'bold',
+       
+    }
 })
 
 export default ResultDetails
